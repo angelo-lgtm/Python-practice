4e30abba70f197d4c64755522ac86cfd7a232ac9
angelo-lgtm <nshutojoseph34@gmail.com>
2026-09-10 14:45:02 +0200

    New codes to learn OOP in python

---
+++ OOP.py
@@ -0,0 +1,23 @@
+class Dog:
+	def __init__(self, name, breed, owner):
+			self.name = name
+			self.breed = breed
+			self.owner = owner
+	
+	def bark(self):
+			print("Whoof whoof")
+			
+class Owner:
+	def __init__(self, name, address, contact_number):
+			self.name = name
+			self._address = address
+			self. phone_number = contact_number
+		
+owner = Owner("Chris", "123 St.Peter Square", "998-9384-03")	
+dog = Dog("Max", "Bulldog", owner)
+print(dog.name)
+dog.bark()
+print(dog.breed)
+
+#This way is not recommended in Python development instead we have to use getters and setters to do this operation.
+print(owner._address) 
