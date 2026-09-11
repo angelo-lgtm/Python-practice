24eddc073694455c795fd1c42fffe2cf74e1d858
angelo-lgtm <nshutojoseph34@gmail.com>
2026-09-11 08:09:27 +0200

    Included static methods, private and protected methods

--- GetterSetterProperty.py
+++ GetterSetterProperty.py
@@ -30,11 +30,66 @@ class Employee:
             
     def description(self):
             print(f"Name of Employee: {self.user.name}\nEmail: {self.user.email}\n")
+
+class BankAccount:
+    balance = 0
+    
+    def __init__(self, employee):
+        self.employee = employee
+        Bank.total_accounts()
+    
+    #Private method created by prefixing the name of method by 2 underscores
+    def __log_transaction(self, type, amount):
+        if type == "withdraw":
+            self.balance -= amount
+        else:
+            self.balance += amount
+    
+    #Procted method created by prefixing the name of method by 1 underscore        
+    def _is_valid_amount(self, amount):
+        return amount > 0
+    
+    def deposit(self, amount):
+        if self._is_valid_amount(amount):
+            self.__log_transaction("deposit", amount)
+            print(f"The amount of {amount} has been deposited into {self.employee.user.name}'s account.")
+        else:
+            print("Transactino failed!")
             
-user = User("Angelo", "20-03-2009", "nshutijoseph34@gmai.com")
-user.email = "bana@gmail.com"
-print(user.email)
+    def withdraw(self, amount):
+        if self._is_valid_amount(amount) and amount <= self.balance:
+            self.__log_transaction("withdraw", amount)
+            print(f"The amount of {amount} has been withdrawn from {employee.user.name}'s account.")
+        else:
+            print("Transaction failed!")
+
+class Bank:
+    accounts = 0
+    
+    #Creation of a static method using the below annotation
+    @staticmethod
+    def total_accounts():
+        Bank.accounts += 1
+        
+    def show_accounts():
+        print(f"The total number of accounts: {Bank.accounts}")
+            
+user = User("Angelo", "20-03-2009", "joseph34@gmai.com")
+
+user2 = User("Divin", "16-06-2007", "divin@gmail.com")
 
 employee = Employee(user, "IT")
 employee.description()
+
+employee2 = Employee(user2, "HR")
+employee2.description()
+
 print(f"Total number of employees: {Employee.e_count}")
+
+account = BankAccount(employee)
+account.deposit(2000000)
+
+account2 = BankAccount(employee2)
+account2.deposit(5000000)
+
+Bank.show_accounts()
